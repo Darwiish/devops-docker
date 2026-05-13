6f07b479b43d0af9ba74ed8a089f84d90b2e6d37
Darwiish <inaaboowe@gmail.com>
2026-05-14 01:25:38 +0400

    Refactor Flask app to use environment variables and improve Docker setup

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,21 +1,23 @@
 from flask import Flask
 import MySQLdb
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # Connect to the MySQL database
     db = MySQLdb.connect(
-        host="mydb",    # Hostname of the MySQL container
-        user="root",    # Username to connect to MySQL
-        passwd="my-secret-pw",  # Password for the MySQL user
-        db="mysql"      # Name of the database to connect to
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        passwd=os.getenv("MYSQL_PASSWORD"),
+        db=os.getenv("MYSQL_DATABASE")
     )
+
     cur = db.cursor()
     cur.execute("SELECT VERSION()")
     version = cur.fetchone()
-    return f'Hello, World! MySQL version: {version[0]}'
+
+    return f"Hello, World! MySQL version: {version[0]}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
